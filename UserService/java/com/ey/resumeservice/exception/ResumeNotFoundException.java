0abfedc0590a5125c4dc517fa87d8878b75bc9aa
ArchanaaS_17 <94780258+ArchanaaS71@users.noreply.github.com>
2026-01-20 17:46:13 +0530

    Delete UserService directory

--- UserService/java/com/ey/resumeservice/exception/ResumeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ey.resumeservice.exception;
-
-public class ResumeNotFoundException extends RuntimeException {
-
-	public ResumeNotFoundException(String message) {
-		super(message);
-	}
-}
